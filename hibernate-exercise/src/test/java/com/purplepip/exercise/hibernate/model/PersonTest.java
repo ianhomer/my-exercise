d17de9607fbe7252a64f123df61151fb5961713a
Ian Homer <ian.homer@gmail.com>
2018-02-17 14:04:05 +0000

    Hibernate Exercise scaffolding

---
+++ hibernate-exercise/src/test/java/com/purplepip/exercise/hibernate/model/PersonTest.java
@@ -0,0 +1,30 @@
+/*
+ * Copyright (c) 2017 the original author or authors. All Rights Reserved
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.purplepip.exercise.hibernate.model;
+
+import static org.junit.Assert.assertEquals;
+
+import com.purplepip.exercise.hibernate.Person;
+import org.junit.Test;
+
+public class PersonTest {
+  @Test
+  public void testPerson() {
+    Person person = new Person();
+    person.setName("test");
+    assertEquals("test", person.getName());
+  }
+}
